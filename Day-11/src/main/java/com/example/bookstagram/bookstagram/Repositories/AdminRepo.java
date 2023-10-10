89690bb792e5fc532005163eb9c31c7916e9e834
Jayanthan007 <120249882+Jayanthan007@users.noreply.github.com>
2023-10-10 09:12:02 +0530

    Add files via upload

---
+++ Day-11/src/main/java/com/example/bookstagram/bookstagram/Repositories/AdminRepo.java
@@ -0,0 +1,14 @@
+package com.example.bookstagram.bookstagram.Repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.bookstagram.bookstagram.Models.Admin;
+
+public interface AdminRepo extends JpaRepository<Admin, Integer>{
+
+    public Optional<Admin> findByAdminemailAndAdminpassword(String adminemail, String adminpassword);
+
+    
+}
